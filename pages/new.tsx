8056b26648f21a3c72e03276c32aa972bea2565b
SulfurBottom <397love@gmail.com>
2022-06-02 09:09:58 +0900

    기본 카드아이템 양식 추가

--- pages/new.tsx
+++ pages/new.tsx
@@ -1,12 +1,49 @@
 import {
   Card,
+  CardActionArea,
+  CardActions,
   CardContent,
-  CardHeader,
+  Chip,
   Divider,
+  IconButton,
   Typography,
 } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import FlagIcon from "@mui/icons-material/Flag";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
 import { NextPage } from "next";
 import Head from "next/head";
+
+const ItemCard: NextPage = () => {
+  return (
+    <Card>
+      <CardActionArea>
+        <CardContent>
+          <Typography variant="h4">
+            <span className="font-semibold">Text Content</span>
+          </Typography>
+          <Typography variant="body1">
+            <span>Urls</span>
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className="flex">
+        <div className="flex-1">
+          <Chip label="battlepage" />
+        </div>
+        <IconButton>
+          <DeleteIcon />
+        </IconButton>
+        <IconButton>
+          <FlagIcon />
+        </IconButton>
+        <IconButton>
+          <BookmarkIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
 
 const New: NextPage = () => {
   return (
@@ -15,22 +52,24 @@ const New: NextPage = () => {
         <title>New</title>
       </Head>
       <div id="info_container" className="flex items-end">
-        <Typography variant="h4" className="flex-1 font-bold">
+        <Typography variant="h3" className="flex-1 font-extrabold">
           New
         </Typography>
         <div id="info_breadcumb" className="">
           <Typography variant="h6">
-            Items: <span className="font-bold">0</span>
+            Items:{" "}
+            <span id="item_count" className="font-bold">
+              0
+            </span>
           </Typography>
         </div>
       </div>
       <Divider />
-      <div id="item_container">
-        <Card>
-          <CardContent>
-            <CardHeader></CardHeader>
-          </CardContent>
-        </Card>
+      <div id="item_container" className="mt-5 flex flex-col gap-5">
+        {[...Array(10)].map((_, i) => (
+          <ItemCard key={i} />
+        ))}
+        <ItemCard />
       </div>
     </div>
   );
