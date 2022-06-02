5cd59afef37c17a6467f1744018adfda7854546f
SulfurBottom <397love@gmail.com>
2022-06-02 14:11:22 +0900

    기본 Data Fetching

--- pages/new.tsx
+++ pages/new.tsx
@@ -13,41 +13,31 @@ import FlagIcon from "@mui/icons-material/Flag";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
 import { NextPage } from "next";
 import Head from "next/head";
+import { apiUrl } from "../store/common";
+import { useState } from "react";
+import Link from "next/link";
 
-const ItemCard: NextPage = () => {
-  return (
-    <Card>
-      <CardActionArea>
-        <CardContent>
-          <Typography variant="h4">
-            <span className="font-semibold">Text Content</span>
-          </Typography>
-          <Typography variant="body1">
-            <span>Urls</span>
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions className="flex">
-        <div className="flex-1">
-          <Chip label="battlepage" />
-        </div>
-        <IconButton>
-          <DeleteIcon />
-        </IconButton>
-        <IconButton>
-          <FlagIcon />
-        </IconButton>
-        <IconButton>
-          <BookmarkIcon />
-        </IconButton>
-      </CardActions>
-    </Card>
-  );
-};
+interface NewProps {
+  id: string;
+  items: ItemProps[];
+}
 
-const New: NextPage = () => {
+interface ItemProps {
+  id: string;
+  text_content: string;
+  url: string;
+  domain: string;
+  tags: string[];
+  timestamp: string;
+  source: string;
+  path: string;
+}
+
+const New: NextPage<NewProps> = (props: NewProps) => {
+  const [currentPage, setCurrentPage] = useState(1);
+
   return (
-    <div className="container flex flex-col p-10">
+    <div className="container flex flex-col p-3 sm:p-10">
       <Head>
         <title>New</title>
       </Head>
@@ -66,13 +56,59 @@ const New: NextPage = () => {
       </div>
       <Divider />
       <div id="item_container" className="mt-5 flex flex-col gap-5">
-        {[...Array(10)].map((_, i) => (
-          <ItemCard key={i} />
+        {props.items.map((item, idx) => (
+          <ItemCard key={idx} {...item} />
         ))}
-        <ItemCard />
       </div>
     </div>
   );
 };
+
+const ItemCard: NextPage<ItemProps> = (item: ItemProps) => {
+  return (
+    <Card>
+      <Link href={item.url} passHref>
+        <a target={`_blank`}>
+          <CardActionArea>
+            <CardContent>
+              <Typography variant="h4">
+                <span className="font-semibold">
+                  {item.text_content !== "" ? item.text_content : item.domain}
+                </span>
+              </Typography>
+              <Typography variant="body1">
+                <span>{item.url}</span>
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </a>
+      </Link>
+
+      <CardActions className="flex">
+        <div className="flex-1">
+          <Chip label={item.domain} />
+        </div>
+        <IconButton>
+          <DeleteIcon />
+        </IconButton>
+        <IconButton>
+          <FlagIcon />
+        </IconButton>
+        <IconButton>
+          <BookmarkIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+
+export async function getServerSideProps() {
+  const res = await fetch(apiUrl + "/items/new");
+  const items = await res.json();
+
+  return {
+    props: { items },
+  };
+}
 
 export default New;
--- store/common.ts
+++ store/common.ts
@@ -1,8 +1,10 @@
 import { atom } from "recoil";
 
+const apiUrl = "https://applemint-go-2gyldlncea-an.a.run.app";
+
 const drawerState = atom({
   key: "drawerState",
   default: false,
 });
 
-export { drawerState };
+export { drawerState, apiUrl };
