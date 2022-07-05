62f46a1bf4661d394d68fbca7e4dd279199908f6
SulfurBottom <397love@gmail.com>
2022-07-05 16:36:16 +0900

    apiUrl Target 변경

--- store/common.ts
+++ store/common.ts
@@ -1,8 +1,8 @@
 import { atom } from "recoil";
 import { ItemProps } from "../components/api";
 
-// const apiUrl = "https://applemint-go-2gyldlncea-an.a.run.app";
-const apiUrl = "http://localhost:8080";
+const apiUrl = "https://applemint-go-2gyldlncea-an.a.run.app";
+// const apiUrl = "http://localhost:8080";
 
 const drawerState = atom({
   key: "drawerState",
